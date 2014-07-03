df4286423556fb8fa3f614c039cd2da633cb9bee
ChanderG <chandergovind@gmail.com>
2014-07-03 12:43:12 +0530

    Base, chapter 1-2

---
+++ README.md
@@ -0,0 +1,10 @@
+#Scheme Compiler in Haskell(WIP)
+
+###What?
+My implementation as I follow along the ["Write yourself a Scheme in 48 hours"](http://jonathan.tang.name/files/scheme_in_48/tutorial/overview.html)
+
+###Run Instructions
+```
+ghc -o main hello.hs
+./main [args]
+```
---
+++ main.hs
@@ -0,0 +1,21 @@
+module Main where
+import System.Environment 
+
+--statements of do need to be aligned
+main = do {-
+          args <- getArgs
+          argst <- getArgs  --any number of getArgs gets the same args passed in 
+          putStrLn("Hello " ++ args !! 0)
+          putStrLn("Hello " ++ argst !! 1)
+          -}
+          
+          --empty lines in between work 
+          putStrLn("Enter your name: ")
+          name <- getLine
+          putStrLn("Hello, " ++ name) 
+ 
+          {-
+          args <- getArgs
+          putStrLn("Bye " ++  show(read(args !! 0) + 10))
+          -} 
+          
