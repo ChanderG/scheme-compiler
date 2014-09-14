1f143d2ec0f0b3c722aa72f1fec3875bc1b64f72
ChanderG <chandergovind@gmail.com>
2014-09-14 18:56:25 +0530

    snd pt of eval2 done

--- src/Compiler.hs
+++ src/Compiler.hs
@@ -82,6 +82,11 @@ eval val@(String _) = return val
 eval val@(Number _) = return val
 eval val@(Bool _) = return val
 eval (List [Atom "quote", val]) = return val
+eval (List [Atom "if", pred, conseq, alt]) = 
+     do result <- eval pred
+        case result of 
+	    Bool False -> eval alt
+	    otherwise -> eval conseq
 eval (List (Atom func : args)) = mapM eval args >>= apply func
 eval badForm = throwError $ BadSpecialForm "Unrecogonized special form" badForm
 
@@ -193,6 +198,7 @@ trapError action = catchError action (return . show)
 --designed to work only with correct values
 extractValue :: ThrowsError a -> a
 extractValue (Right val) = val
+
 
 --Main
 --main :: IO()
