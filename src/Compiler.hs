115db7d254d32de297578b6bcde7a9c9c93d9a8e
ChanderG <chandergovind@gmail.com>
2014-09-21 22:49:23 +0530

    before readContents

--- src/Compiler.hs
+++ src/Compiler.hs
@@ -275,13 +275,31 @@ ioPrimitives :: [(String, [LispVal] -> IOThrowsError LispVal)]
 ioPrimitives = [("apply", applyProc),
                 ("open-input-file", makePort ReadMode),
                 ("open-output-file", makePort WriteMode),
-                ("close-input-port", makePort ),
-                ("close-output-port", makePort ),
+                ("close-input-port", closePort ),
+                ("close-output-port", closePort ),
 		("read", readProc),
 		("write", writeProc),
 		("read-contents", readContents),
 		("read-all", readAll)]
 
+applyProc :: [LispVal] -> IOThrowsError LispVal
+applyProc [func, List args] = apply func args
+applyProc (func : args) = apply func args
+
+makePort :: IOMode -> [LispVal] -> IOThrowsError LispVal
+makePort mode [String filename] = liftM Port $ liftIO $ openFile filename mode
+
+closePort :: [LispVal] -> IOThrowsError LispVal
+closePort [Port filename] = liftIO $ hClose port >> (return $ Bool True)
+closePort _ = return $ Bool False
+
+readProc :: [LispVal] -> IOThrowsError LispVal
+readProc [] = readProc [Port stdin] 
+readProc [Port port] = (liftIO $ hGetLine port) >>= liftThrows . readExpr
+
+writeProc :: [LispVal] -> IOThrowsError LispVal
+writeProc [obj] = writeProc [obj, Port stdout] 
+writeProc [obj, Port port] = liftIO $ hPrint port obj >> (return $ Bool True) 
 
 numericBinop :: (Integer -> Integer -> Integer) -> [LispVal] ->ThrowsError LispVal
 numericBinop op [] = throwError $ NumArgs 2 [] 
